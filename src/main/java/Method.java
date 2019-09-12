38111334477f44137818b4377fd7115f534634ad
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-09-12 19:46:45 -0400

    Separate the arguments into classes

---
+++ src/main/java/Method.java
@@ -0,0 +1,8 @@
+public class Method {
+
+    private String method[];
+
+    public Method(String[] method) {
+        this.method = method;
+    }
+}
--- src/main/java/httpc.java
+++ src/main/java/httpc.java
@@ -2,26 +2,7 @@ public class httpc {
 
     public static void main(String[] args) {
 
-    }
 
-    //general help menu
-    public void help() {
-    }
-
-    //get help menu
-    public void helpGET() {
-    }
-
-    //post help menu
-    public void helpPOST() {
-    }
-
-    //get operation
-    public void get() {
-    }
-
-    //post operation
-    public void post() {
     }
 
 }
