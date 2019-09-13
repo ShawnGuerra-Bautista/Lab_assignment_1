2fac6595a42d99ff2cf3e636d58e7a095ebf5adb
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-09-13 18:43:13 -0400

    Added if statements for other methods

--- src/main/java/Help.java
+++ src/main/java/Help.java
@@ -2,24 +2,24 @@ public class Help {
 
     private String[] arguments;
 
-    public Help(String[] arguments){
+    public Help(String[] arguments) {
         this.arguments = arguments;
         parseArguments(this.arguments);
     }
 
-    private void parseArguments(String[] arguments){
-        if(arguments.length == 1 && arguments[0].equals("help")){
+    private void parseArguments(String[] arguments) {
+        if(arguments.length == 1 && arguments[0].equals("help")) {
             System.out.println(generalHelpMessage());
-        }else if(arguments.length == 2 && arguments[0].equals("help") && arguments[1].equals("get")){
+        }else if(arguments.length == 2 && arguments[0].equals("help") && arguments[1].equals("get")) {
             System.out.println(getHelpMessage());
-        }else if(arguments.length == 2 && arguments[0].equals("help") && arguments[1].equals("post")){
+        }else if(arguments.length == 2 && arguments[0].equals("help") && arguments[1].equals("post")) {
             System.out.println(postHelpMessage());
-        }else{
+        }else {
             System.out.println("Invalid command.");
         }
     }
 
-    private String generalHelpMessage(){
+    private String generalHelpMessage() {
         return "\nhttpc is a curl-like application but supports HTTP protocol only" +
                 "\nUsage: httpc command [arguments]" +
                 "\nThe commands are:" +
@@ -29,7 +29,7 @@ public class Help {
                 "\n\nUse \"httpc help [command]\" for more information about a command.";
     }
 
-    private String getHelpMessage(){
+    private String getHelpMessage() {
         //Can be replace with parser + handler, but (for now) this works fine
         return "\nUsage: httpc get [-v] [-h key:value] URL" +
                 "\n\nGet executes a HTTP GET request for a given URL." +
@@ -37,7 +37,7 @@ public class Help {
                 "\n\t-h key:value\tAssociates headers to HTTP Request with the format 'key:value'.";
     }
 
-    private String postHelpMessage(){
+    private String postHelpMessage() {
         //Can be replace with parser + handler, but (for now) this works fine
         return "\nUsage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL" +
                 "\n\nPost executes a HTTP POST request for a given URL with inline data or from file." +
--- src/main/java/httpc.java
+++ src/main/java/httpc.java
@@ -3,8 +3,12 @@ public class httpc {
     public static void main(String[] args) {
         if (args[0].equals("help")) {
             new Help(args);
-        } else {
-
+        }else if(args[0].equals("get")) {
+            return;
+        }else if(args[0].equals("post")) {
+            return;
+        }else {
+            System.out.println("Invalid Command.");
         }
 
     }
