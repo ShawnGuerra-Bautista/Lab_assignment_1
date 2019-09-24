aa82f9af483e4c6617e879c31936f730ea524bda
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-09-23 21:06:18 -0400

    Implemented parsing for fileResponseOption
    Co-authored-by: wuwentang <wuwen_tang@hotmail.com>

--- src/main/java/GETRequest.java
+++ src/main/java/GETRequest.java
@@ -19,10 +19,6 @@ public class GETRequest extends Request {
     public OptionParser getGETparser() {
         OptionParser parser = new OptionParser();
         parser.nonOptions("Method GET").ofType(String.class);
-        parser.accepts("v", "Prints the detail of the response such as protocol, " +
-                "status, and headers.");
-        parser.accepts("h", "Associates headers to HTTP Request with the " +
-                "format 'key:value'").withRequiredArg();
         parser.nonOptions("URL").ofType(String.class);
         return parser;
     }
--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -2,7 +2,10 @@ import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
+
+import static java.util.Arrays.asList;
 
 //All requests (POST/GET) should support
 //      1. -o option
@@ -17,8 +20,22 @@ public abstract class Request {
     }
 
     private Map<String, String> headerOption(){
-        //Should return a hashmap
-        return null;
+        OptionParser parser = new OptionParser();
+        parser.accepts("h", "Associates headers to HTTP Request with the format 'key:value'")
+                .withRequiredArg()
+                .ofType(String.class);
+        OptionSet headerOption = parser.parse(args);
+        Object header = headerOption.valueOf("h");
+
+        //Find out if the Object from headerOption is a String or a List to create the appropriate HashMap
+        if(header instanceof String){
+            return null;
+        }else if(header instanceof List){
+            //List<String> headerList = headerOption.valueOf("List");
+            return null;
+        }else{
+            return null;
+        }
     }
     private boolean verboseOption(){
         OptionParser parser = new OptionParser();
@@ -27,7 +44,10 @@ public abstract class Request {
         return verboseOption.has("v");
     }
     private File fileResponseOption(){
-        //Should return a File for FileOutputStream
+        OptionParser parser = new OptionParser();
+        parser.accepts("o", "Prints the detail of the response in a file.").withRequiredArg().ofType(String.class);
+        OptionSet fileResponseOption = parser.parse(args);
+        File fileResponse = new File((String)fileResponseOption.valueOf("o"));
         return null;
     }
 }
