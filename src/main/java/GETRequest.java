9c796eaf01aeae6d949365273bfeb6a99f27f4d2
wuwentang <wuwen_tang@hotmail.com>
2019-09-26 16:17:36 -0400

    add HTTP request to web server

--- src/main/java/GETRequest.java
+++ src/main/java/GETRequest.java
@@ -4,7 +4,10 @@
             >Header (Content-length not mandatory)
  */
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -17,20 +20,29 @@ public class GETRequest extends Request {
         super(args);
     }
 
-    public void execute() throws UnknownHostException {
+    public void execute() {
+
+
         String url = rawUrl.substring(rawUrl.indexOf("http://"), rawUrl.length()-1);
 
         InetAddress inetAddress = null;
+        Socket serviceSocket = null;
+
         try {
             inetAddress = InetAddress.getByName(url);
-        }catch(UnknownHostException e){
-            System.out.println(e);
-        }
-
-        Socket serviceSocket = null;
-        try {
             serviceSocket = new Socket(inetAddress, 80);
-        }catch(IOException e){
+
+            PrintWriter requestWriter = new PrintWriter(serviceSocket.getOutputStream());
+            BufferedReader responseBuffer = new BufferedReader(
+                    new InputStreamReader(serviceSocket.getInputStream()));
+
+            // send HTTP request to web server
+            requestWriter.println("GET / HTTP/1.0");
+            requestWriter.println("Host: " + url + ":" + "80");
+            requestWriter.println("Connection: close");
+            requestWriter.println();
+
+        } catch(Exception e) {
             System.out.println(e);
         }
     }
