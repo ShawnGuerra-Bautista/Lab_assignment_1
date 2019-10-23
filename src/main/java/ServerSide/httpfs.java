fc12db2dc106517a0f590d469797d8ae481b49f6
wuwentang <wuwen_tang@hotmail.com>
2019-10-23 11:02:07 -0400

    httpfs create http server
    Co-authored-by: Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>

--- src/main/java/ServerSide/httpfs.java
+++ src/main/java/ServerSide/httpfs.java
@@ -1,7 +1,33 @@
 package ServerSide;
 
-public class httpfs {
-    public static void main(String[] args) {
-        
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+/**
+ * Create HTTP Server
+ *
+ */
+
+public class httpfs
+{
+    static int serverPort = 8080;
+
+    public static void main(String[] args) throws IOException{
+
+        try( ServerSocket server = new ServerSocket(serverPort) ){
+            System.out.println("Server has been instantiated at port " + serverPort);
+
+            while(true){
+
+                try ( Socket client_connection = server.accept() )
+                {
+                    PrintWriter outbount_client = new PrintWriter(client_connection.getOutputStream(), true);
+                    outbount_client.println("Well hello to you too.");
+                    client_connection.close();
+                }
+            }
+        }
     }
 }
