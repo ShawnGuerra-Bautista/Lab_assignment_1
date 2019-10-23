4313153956bc9e1c2740fc92517e2deb85de5719
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-10-23 11:41:38 -0400

    Added parsers
    Co-authored-by: wuwentang <wuwen_tang@hotmail.com>

---
+++ src/main/java/ServerSide/ServerRequestHandler.java
@@ -0,0 +1,61 @@
+package ServerSide;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+import java.io.File;
+
+public class ServerRequestHandler {
+    private String[] args;
+
+    public ServerRequestHandler(String[] args){
+        this.args = args;
+    }
+
+    public void handle(){
+
+    }
+
+    public boolean debugOption(){
+        OptionParser parser = new OptionParser();
+        parser.accepts("v", "Prints debugging messages");
+        parser.allowsUnrecognizedOptions();
+        OptionSet debugOption = parser.parse(args);
+        return debugOption.has("v");
+    }
+
+    public int portOption(){
+        OptionParser parser = new OptionParser();
+        OptionSpec<Integer> portSpec = parser.accepts("p", "Specifies the port number that the server" +
+                " will listen and server at.\nDefault is 8080.")
+                .withRequiredArg()
+                .ofType(Integer.class);
+        parser.allowsUnrecognizedOptions();
+        OptionSet portResponseOption = parser.parse(args);
+
+        Integer portResponse = 8080;
+
+        if(portResponseOption.has("p")){
+            portResponse = portResponseOption.valueOf(portSpec);
+        }
+        return portResponse;
+    }
+
+    public File pathOption(){
+        OptionParser parser = new OptionParser();
+        OptionSpec<String> pathSpec = parser.accepts("d", "Specifies the directory that the server " +
+                "will use to read/write requested files. Default is the current directory " +
+                "when launching the application.")
+                .withRequiredArg()
+                .ofType(String.class);
+        parser.allowsUnrecognizedOptions();
+        OptionSet pathResponseOption = parser.parse(args);
+
+        File pathResponse = null;
+        if(pathResponseOption.has("d")){
+            pathResponse = new File(pathResponseOption.valueOf(pathSpec).replaceAll("[\"']", ""));
+        }
+        return pathResponse;
+    }
+}
--- src/main/java/ServerSide/httpfs.java
+++ src/main/java/ServerSide/httpfs.java
@@ -1,37 +1,32 @@
 package ServerSide;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.PrintWriter;
-import java.io.IOException;
-
-/**
- * Create HTTP Server
- *
- */
-
 public class httpfs
 {
     static int serverPort = 8080;
 
     public static void main(String[] args) {
 
-        ServerSocket server = null;
-        try {
-            server = new ServerSocket(serverPort);
-        }catch(IOException e) {
-            System.out.println(e);
-        }
+        ServerRequestHandler handler = new ServerRequestHandler(args);
+        handler.handle();
 
-        //Accept requests
-        Socket client;
-        while(true){
-            //Accept Requests
+        /*
+            ServerSocket server = null;
             try {
-                client = server.accept();
-            }catch(IOException e){
+                server = new ServerSocket(serverPort);
+            }catch(IOException e) {
                 System.out.println(e);
             }
-        }
+
+            //Accept requests
+            Socket client;
+            while(true){
+                //Accept Requests
+                try {
+                    client = server.accept();
+                }catch(IOException e){
+                    System.out.println(e);
+                }
+            }
+         */
     }
 }
